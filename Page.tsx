4150ffb1ae9a6a5893cd01aa0579a3e918f7c431
mtornani <mirkotornani@gmail.com>
2025-05-28 09:12:57 +0200

    Create Page.tsx

---
+++ Page.tsx
@@ -0,0 +1,12 @@
+```jsx
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl font-bold">
+        BleedTime
+        <span className="text-red-600 block">Coming Soon</span>
+      </h1>
+    </div>
+  )
+}
+```
