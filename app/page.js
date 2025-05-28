47c55f5f93cf7ae56afd741f2ac04b80aeb7866a
mtornani <mirkotornani@gmail.com>
2025-05-28 14:12:57 +0200

    Update and rename page.tsx to page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: 'black',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1 style={{fontSize: '4rem'}}>
+        BleedTime
+      </h1>
+    </div>
+  )
+}
